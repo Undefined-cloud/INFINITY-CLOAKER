b9ebb6c58a331d82a67978eae52f48433c30a788
SchoolHacker <122331032+SchoolHacker3@users.noreply.github.com>
2023-04-15 15:57:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,2 @@
 javascript:var popup = window.open("about:blank", "popup", "scrollbars=1,height=" + screen.availHeight + ",width=" + screen.availWidth);
-popup.document.write("<html><head></head><body style='margin: 0px; height: 100vh;'><iframe allow='fullscreen' src='https://b.lavaiswet.ml/' style='border: none; width: 100%; height: 100%; margin: 0px;'></iframe></body></html>");
+popup.document.write("<html><head></head><body style='margin: 0px; height: 100vh;'><iframe allow='fullscreen' src='https://lolwelikesubdomains.pakasak.com/' style='border: none; width: 100%; height: 100%; margin: 0px;'></iframe></body></html>");
